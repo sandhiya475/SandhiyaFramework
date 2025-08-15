cf7bb9d0e0c834186d8d322112fef1ad548a3de7
gurra <gurra@gurramprasunsai>
2025-08-15 16:31:45 +0530

    written contacts adding vijetha test scripts.second commited contact after organization

--- sandhiyaProject/src/test/java/organizationTest/ToCreateOrganizationTest.java
+++ sandhiyaProject/src/test/java/organizationTest/ToCreateOrganizationTest.java
@@ -5,6 +5,7 @@ public class ToCreateOrganizationTest {
 	public static void main(String[] args) {
 
 		System.out.println("Organization Created Successfully");
+		System.out.println("-----vijetha succuss-----------");
 	}
 
 }
---
+++ sandhiyaProject/src/test/java/vijethatest/CreatedVijthaTest.java
@@ -0,0 +1,10 @@
+package vijethatest;
+
+public class CreatedVijthaTest {
+
+	public static void main(String[] args) {
+		System.out.println("-----vijetha------");
+
+	}
+
+}
