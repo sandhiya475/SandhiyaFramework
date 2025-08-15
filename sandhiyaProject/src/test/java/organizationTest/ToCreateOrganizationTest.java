45f310822edd024ec354776704592f341a659a63
SANDHIYA <SANDHIYA@DESKTOP-IRHS2JI>
2025-08-15 08:33:22 +0530

    Sharing framework

---
+++ sandhiyaProject/src/test/java/organizationTest/ToCreateOrganizationTest.java
@@ -0,0 +1,10 @@
+package organizationTest;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Organization Created Successfully");
+	}
+
+}
